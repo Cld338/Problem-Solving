6783d744828c141cdd1ceff2e54aef6000a8dceb
오지훈 <jihuno291@gmail.com>
2024-09-25 19:07:24 +0900

    [Gold V] Title: 암호, Time: 300 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Gold/1394. 암호/암호.py
@@ -0,0 +1,24 @@
+import sys
+
+def main():
+    texts = sys.stdin.readline().strip()
+    ans = sys.stdin.readline().strip()
+    num = len(texts)
+    mod = 900528
+    length = len(ans)
+    char_to_index = {char: idx for idx, char in enumerate(texts)}
+    c = 0
+    power = 1
+    for _ in range(1, length):
+        power = (power * num) % mod
+        c = (c + power) % mod
+    power = 1
+    for i in reversed(range(length)):
+        index = char_to_index[ans[i]]
+        c = (c + index * power) % mod
+        power = (power * num) % mod
+
+    print((c + 1) % mod)
+
+if __name__ == "__main__":
+    main()
