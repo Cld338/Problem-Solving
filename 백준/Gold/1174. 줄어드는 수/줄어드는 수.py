fd14391a51609358b00ea8d9038ed8c2a7abf7ef
오지훈 <jihuno291@gmail.com>
2024-09-25 14:06:01 +0900

    [Gold V] Title: 줄어드는 수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
@@ -1,18 +1,17 @@
-queue=[i for i in range(10)]
-next_queue = []
+q=list(range(10))
+p=[]
 a=int(input())
 n=0
 while True:
-    if not(queue):
-        queue=next_queue
-        next_queue=[]
-    if not(queue):
+    if not(q):
+        q,p=p,[]
+    if not(q):
         print(-1)
         break
-    x = queue.pop(0)
+    x=q.pop(0)
     n+=1
     if n==a:
         print(x)
         break
     for i in range(x%10):
-        next_queue.append(10*x+i)
+        p.append(10*x+i)
