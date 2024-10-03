4853193d412c4424c8cb79313b6ff0dc5ad698c0
오지훈 <jihuno291@gmail.com>
2024-10-03 16:12:21 +0900

    [Silver III] Title: 킹, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -2,74 +2,24 @@ def check_valid(x,y):
     if abs(x-4.5)>=4 or abs(y-4.5)>=4:
         return False
     return True
-
-class King():
-    def __init__(self, x, y):
-        self.x=x
-        self.y=y
-    def move(self, direction):
-        if direction=="T":
-            self.y+=1
-        elif direction=="B":
-            self.y-=1
-        elif direction=="L":
-            self.x-=1
-        elif direction=="R":
-            self.x+=1
-        elif direction=="LT":
-            self.x-=1
-            self.y+=1
-        elif direction=="RT":
-            self.x+=1
-            self.y+=1
-        elif direction=="LB":
-            self.x-=1
-            self.y-=1
-        elif direction=="RB":
-            self.x+=1
-            self.y-=1
-    def check_valid(self, direction):
-        if direction=="T":
-            if check_valid(self.x, self.y+1):
-                return True
-        elif direction=="B":
-            if check_valid(self.x, self.y-1):
-                return True
-        elif direction=="L":
-            if check_valid(self.x-1, self.y):
-                return True
-        elif direction=="R":
-            if check_valid(self.x+1, self.y):
-                return True
-        elif direction=="LT":
-            if check_valid(self.x-1, self.y+1):
-                return True
-        elif direction=="RT":
-            if check_valid(self.x+1, self.y+1):
-                return True
-        elif direction=="LB":
-            if check_valid(self.x-1, self.y-1):
-                return True
-        elif direction=="RB":
-            if check_valid(self.x+1, self.y-1):
-                return True
-        return False
-
-class Stone(King):
-    pass
-
 king_pos,stone_pos,N=input().split()
-king=King(ord(king_pos[0])-64, int(king_pos[1]))
-stone=Stone(ord(stone_pos[0])-64, int(stone_pos[1]))
+king_x, king_y=ord(king_pos[0])-64, int(king_pos[1])
+stone_x, stone_y=ord(stone_pos[0])-64, int(stone_pos[1])
 for i in range(int(N)):
     direction=input()
-    if king.check_valid(direction):
-        prev_x, prev_y=(king.x,king.y)
-        king.move(direction)
-        if king.x==stone.x and king.y==stone.y:
-            if stone.check_valid(direction):
-                stone.move(direction)
+    T=("T" in direction)
+    B=("B" in direction)
+    L=("L" in direction)
+    R=("R" in direction)
+    if check_valid(king_x - L +  R, king_y + T -  B):
+        prev_x, prev_y=(king_x,king_y)
+        king_x+=R-L
+        king_y+=T-B
+        if king_x==stone_x and king_y==stone_y:
+            if check_valid(stone_x - L +  R, stone_y + T -  B):
+                stone_x += - L +  R
+                stone_y += + T -  B
             else:
-                king.x,king.y=prev_x,prev_y
-print(chr(king.x+64)+str(king.y))
-print(chr(stone.x+64)+str(stone.y))
+                king_x,king_y=prev_x,prev_y
+print(chr(king_x+64)+str(king_y))
+print(chr(stone_x+64)+str(stone_y))
