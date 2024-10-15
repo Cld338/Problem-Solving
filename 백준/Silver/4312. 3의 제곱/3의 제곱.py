a6f4cc2cd30eae24ab62bfcadae7c78a6df91af5
오지훈 <jihuno291@gmail.com>
2024-10-15 15:53:22 +0900

    [Silver III] Title: 3의 제곱, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4312. 3의 제곱/3의 제곱.py
+++ 백준/Silver/4312. 3의 제곱/3의 제곱.py
@@ -2,10 +2,12 @@ import sys
 input=sys.stdin.readline
 t=int(input())
 while t:
-    n = bin(t-1)[2:]
+    t=t-1
     ls = []
-    for i in range(len(n)):
-        if int(n[i]):
-            ls.append(str(3**(len(n)-i-1) * int(n[i])))
-    print("{ }" if t==1 else "{ " + ", ".join(ls[::-1])+ " }")
+    i=0
+    for i in range(len(bin(t))-2):
+        if t&(1<<i):
+            ls.append(3**i)
+        i+=1
+    print("{ " + ", ".join(map(str, ls))+ " }" if t else "{ }")
     t = int(input())
